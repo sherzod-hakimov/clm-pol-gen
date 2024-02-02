2f29896738ce864a002da7e0ce77434e208722b6
Sherzod Hakimov <sherzodhakimov@gmail.com>
2024-02-02 14:31:26 +0100

    instance gen

--- games/cloudgame/instancegenerator.py
+++ games/cloudgame/instancegenerator.py
@@ -1,5 +1,6 @@
 
 import sys
+import json
 
 #sys.path.append("/Users/antonia/Documents/Unizeug/2023_SoSe_IM/clembench")
 
@@ -14,20 +15,56 @@ class CloudGameInstanceGenerator(GameInstanceGenerator):
         # always do this to initialise GameInstanceGenerator
         super().__init__(GAME_NAME)
     def on_generate(self):
-      
-        prompt = self.load_template('resources/initial_prompts/prompt.template')
-        experiments = {"no" : self.load_file('resources/no_clouds.txt').strip('\n').split('\n'), 
-                 "yes": self.load_file('resources/yes_clouds.txt').strip('\n').split('\n') }
 
-        for exp in experiments.keys():
-             experiment = self.add_experiment(exp)
-             game_id = 0
-             for inst in experiments[exp]:
-                 game_id = game_id
-                 instance = self.add_game_instance(experiment, game_id)
-                 instance["image"] = "games/cloudgame/resources/images/" + inst
-                 instance["prompt"] = prompt
-                 game_id += 1
+        player_a_prompt_header = self.load_template(f"resources/prompt.template")
+
+        split_path = 'resources/train_1.jsonl'
+
+        dataset = {}
+        with open(split_path, "r") as file:
+            for l in file.readlines():
+                data = json.loads(l)
+                dataset[data['id']] = data
+                z = 1
+
+        file_path = 'resources/train_iblip_flant5xxl.jsonl'
+
+        experiments = ['prompt_one', 'prompt_two', 'prompt_three', 'prompt_four']
+
+        experiments = ['prompt_one']
+
+        instance_counter = 0
+
+        for experiment in experiments:
+
+            instance_counter = 0
+
+            experiment_name = self.add_experiment('policy_with_iblip_flan_xxl_' + experiment)
+            game_counter = 0
+
+            with open(file_path, "r") as file:
+                for l in file.readlines():
+                    data = json.loads(l)
+
+                    label = dataset[data['id']]['label']
+                    if label != '1':
+                        continue
+
+                    prompt = player_a_prompt_header.replace('$TEXT',
+                                                            dataset[data['id']]['text']).replace(
+                        '$IMAGE_CONTEXT', data['descriptions'][experiment])
+
+                    game_instance = self.add_game_instance(experiment_name, game_counter)
+
+                    game_instance["prompt"] = prompt
+                    game_instance["image"] = "games/cloudgame/resources/images/" + data['id']
+                    game_instance["id"] = data['id']
+
+                    instance_counter += 1
+
+                    if instance_counter == 10:
+                        break
+            print(instance_counter)
 
 if __name__ == '__main__':
     # always call this, which will actually generate and save the JSON file
